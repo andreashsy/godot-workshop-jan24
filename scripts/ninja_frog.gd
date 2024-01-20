58bc65c191c948b4043ea6ca3cbc293d49720067
andreashsy <songyanghan@gmail.com>
2024-01-20 15:30:23 +0800

    add animation

--- scripts/ninja_frog.gd
+++ scripts/ninja_frog.gd
@@ -1,15 +1,33 @@
 class_name NinjaFrog
 extends CharacterBody2D
 
-#@export var gravity: float = 300.0
+@export var SPEED: float = 150.0
+@export var JUMP: float = 250.0
 var gravity = ProjectSettings.get("physics/2d/default_gravity")
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 
 func _ready() -> void:
     pass
 
 func _physics_process(delta) -> void:
+    var dir
+    var anim = "idle"
+    
     if not is_on_floor():
         velocity.y  += gravity * delta
-        
+        anim = "jump" if velocity.y < 0 else "fall"
+    else: 
+        dir = Input.get_axis("left", "right")
+        velocity.x = SPEED * dir
+        if dir == 0:
+            anim = "idle"
+        else:
+            anim = "run"
+            animated_sprite_2d.flip_h = dir < 0
+        if Input.is_action_just_pressed("jump"):
+            velocity.y = -JUMP
+    
     velocity.y = min(velocity.y, gravity)
     move_and_slide()
+    animated_sprite_2d.play(anim)
