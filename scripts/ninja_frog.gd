291da88e3f6f66305ade56be52e2b8d0922af6f4
andreashsy <songyanghan@gmail.com>
2024-01-20 15:41:58 +0800

    add jump sound, platform oneway, limit camera bottom

--- scripts/ninja_frog.gd
+++ scripts/ninja_frog.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 @export var JUMP: float = 250.0
 var gravity = ProjectSettings.get("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 
 func _ready() -> void:
@@ -13,20 +14,20 @@ func _ready() -> void:
 func _physics_process(delta) -> void:
     var dir
     var anim = "idle"
+    dir = Input.get_axis("left", "right")
+    velocity.x = SPEED * dir
     
     if not is_on_floor():
         velocity.y  += gravity * delta
         anim = "jump" if velocity.y < 0 else "fall"
     else: 
-        dir = Input.get_axis("left", "right")
-        velocity.x = SPEED * dir
-        if dir == 0:
-            anim = "idle"
+        if dir == 0: anim = "idle"
         else:
             anim = "run"
             animated_sprite_2d.flip_h = dir < 0
         if Input.is_action_just_pressed("jump"):
             velocity.y = -JUMP
+            audio_stream_player_2d.play()
     
     velocity.y = min(velocity.y, gravity)
     move_and_slide()
